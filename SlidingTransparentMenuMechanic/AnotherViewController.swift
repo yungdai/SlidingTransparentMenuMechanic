da7716809b389ec47752b820ffc5468de6193470
Yung Dai <yungchidai@gmail.com>
2015-06-02 23:16:44 -0400

    synch

---
+++ SlidingTransparentMenuMechanic/AnotherViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AnotherViewController.swift
+//  SlidingTransparentMenuMechanic
+//
+//  Created by Yung Dai on 2015-06-02.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+import UIKit
+
+class AnotherViewController: UIViewController, ENSideMenuDelegate  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Move next line to viewWillAppear functon if you store your view controllers
+        self.sideMenuController()?.sideMenu?.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toggleSideMenu(sender: AnyObject) {
+        toggleSideMenuView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlidingTransparentMenuMechanic/ViewController.swift
+++ SlidingTransparentMenuMechanic/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ENSideMenuDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sideMenuController()?.sideMenu?.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +20,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func toggleSideMenu(sender: AnyObject) {
+        toggleSideMenuView()
+    }
+    
+    // MARK: - ENSideMenu Delegate
+    func sideMenuWillOpen() {
+        println("sideMenuWillOpen")
+    }
+    
+    func sideMenuWillClose() {
+        println("sideMenuWillClose")
+    }
+    
+    func sideMenuShouldOpenSideMenu() -> Bool {
+        println("sideMenuShouldOpenSideMenu")
+        return true
+    }
 
 }
 
